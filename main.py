5414af332a59680878df625d86b739c58facd861
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-01 17:36:25 -0500

    added navigation function

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ print("Hello! Welcome to your contact book!")
 class ContactBook:
     def menu(self):
         print("\nMAIN MENU")
-        response = input("\nAdd a contact: A\nSearch contacts: S\n\n")
+        response = input("\nAdd a contact: A\nSearch contacts: S\nView all contacts: V\n\n")
         if response == "A":
             self.add()
         elif response == "S":
             self.search()
+        elif response == "V":
+            self.view()
         else:
-            print("Sorry, that's not an option.")
+            print("Sorry, that's not an option. Please select a valid option.")
             self.menu()
     def add(self):
         print("\nADD A CONTACT")
@@ -35,7 +37,7 @@ class ContactBook:
         last_name = input("Last Name: ")
         phone_number = input("Phone Number: ")
         new_contact = Contact(first_name = first_name, last_name = last_name, phone_number = phone_number)
-        print("\nYour contact has been saved!\n")
+        print("\nYour contact has been saved! Returning to main menu.\n")
         new_contact.save()
         self.menu()
     def search(self):
@@ -43,14 +45,21 @@ class ContactBook:
         search = input("\nEnter their first name: ")
         result = Contact.select().where(Contact.first_name.contains(search)).get()
         print(f"\n{result.first_name} {result.last_name}\n{result.phone_number}\n")
-        response = input("Return to the main menu: M\nExit contact book: Q\n")
+        self.navigation()
+    def view(self):
+        print("\nCONTACTS")
+        for all_contacts in Contact.select():
+            print(f"\n{all_contacts.first_name} {all_contacts.last_name}\nPhone Number: {all_contacts.phone_number}")
+        self.navigation()
+    def navigation(self):
+        response = input("\nReturn to the main menu: M\nExit contact book: Q\n")
         if response == "M":
             self.menu()
         elif response == "Q":
             return
         else:
-            print("Sorry, that's not an option. Returning to search.")
-            self.search()
+            print("\n\nSorry, that's not an option. Please enter a valid option.")
+            self.navigation()
 
 
 contact_book = ContactBook()
